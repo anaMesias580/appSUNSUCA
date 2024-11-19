7e186761b193ec37c8910e5dbdcc9ba16c8799ac
SantyRxx <santiagor.aprendiz@gmail.com>
2024-11-18 22:43:17 -0500

    repositorioSunsuca

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,15 @@
+
+import"./formulario.css"
+export function Formulario(){
+return(
+        <section>
+            <h1>Login</h1>
+
+            <form className="formulario">
+                <input type="text" />
+                <input type="password" />
+                <button>INICIAR SESION</button>
+            </form>
+        </section>
+    )
+}
